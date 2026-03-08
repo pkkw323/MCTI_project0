6b96574db8cb0e40962e283c75ef799157d6e8ed
pkkw323 <pkkw323@gmail.com>
2026-03-08 00:15:20 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MCTI_project/',
+  base: '/MCTI_project0/',
   server: {
     port: 5173,
   },
